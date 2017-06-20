d06c66d5c1514695f3d1f2422f93df1ccce45bae
Phat Tran <PHAT@Phat.local>
2017-06-21 01:14:35 +0700

    add sw-precache & generate service-worker.js

---
+++ sw-precache-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  navigateFallback: '/index.html',
+  stripPrefix: 'src',
+  root: 'src/',
+  staticFileGlobs: [
+    'src/index.html',
+    'src/**.js',
+    'src/**.css'
+  ]
+};
