4081b71fd28b0ee8065bd2fed5f5c64407b44177
Phat Tran <PHAT@Phat.local>
2017-06-21 13:31:42 +0700

    "yarn upgrade" & remove hammerjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import {
 } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import 'hammerjs';
 
 // App modules & components
 import { AppComponent } from './app.component';
